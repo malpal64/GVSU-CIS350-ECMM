35955171e26c057134c75f3a1d3c3d2405e265a2
elinorkochanski <kochanse@mail.gvsu.edu>
2023-10-31 23:32:05 -0400

    part 21 done

--- src/pygame_rougelike/weapon.py
+++ src/pygame_rougelike/weapon.py
@@ -51,7 +51,7 @@ class Arrow(pygame.sprite.Sprite):
         self.dx = math.cos(math.radians(self.angle)) * constants.ARROW_SPEED
         self.dy = -(math.sin(math.radians(self.angle)) * constants.ARROW_SPEED)
 
-    def update(self, screen_scroll, enemy_list):
+    def update(self, screen_scroll, obstacle_tiles, enemy_list):
         # reset variables
         damage = 0
         damage_pos = 0
@@ -59,6 +59,11 @@ class Arrow(pygame.sprite.Sprite):
         # reposition based on speed
         self.rect.x += screen_scroll[0] + self.dx
         self.rect.y += screen_scroll[1] + self.dy
+
+        # check for collision between arrow and tile walls
+        for obstacle in obstacle_tiles:
+            if obstacle[1].colliderect(self.rect):
+                self.kill()
 
         # check if arrow has gone off screen
         if self.rect.right < 0 or self.rect.left > constants.SCREEN_WIDTH or self.rect.bottom < 0 or self.rect.top > constants.SCREEN_HEIGHT:
@@ -70,6 +75,7 @@ class Arrow(pygame.sprite.Sprite):
                 damage = 10 + random.randint(-5, 5)
                 damage_pos = enemy.rect
                 enemy.health -= damage
+                enemy.hit = True
                 self.kill()
                 break
 
