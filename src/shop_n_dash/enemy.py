6462a3565948649b921cbbf9334b388db9b242d7
elinorkochanski <kochanse@mail.gvsu.edu>
2023-11-19 14:08:03 -0500

    update enemy and player

--- src/shop_n_dash/enemy.py
+++ src/shop_n_dash/enemy.py
@@ -4,3 +4,9 @@ class Enemy():
     def __init__(self, health, attack_power):
         self.health = health
         self.attack_power = attack_power
+
+    def update_enemy(self):
+        pass
+
+    def enemy_ai(self):
+
--- src/shop_n_dash/player.py
+++ src/shop_n_dash/player.py
@@ -10,3 +10,6 @@ class Player():
 
     def draw(self, surface):
         pygame.draw.rect(surface, (255,0,0), self.rect)
+
+    def update_player(self):
+        pass
